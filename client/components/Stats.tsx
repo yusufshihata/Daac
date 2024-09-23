1fe670e2b80ca08d96dff907816684c71e21fcce
yusufshihata <yusufshihata2006@gmail.com>
2024-09-23 08:52:39 +0300

    Start Development

---
+++ client/components/Stats.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import { Container, Text, SimpleGrid, Box, chakra, Stack, Flex, Icon, useColorModeValue } from '@chakra-ui/react';
+import { AiFillStar } from 'react-icons/ai';
+import { FaUsers, FaTwitter } from 'react-icons/fa';
+import { BsPerson } from 'react-icons/bs';
+
+interface StatData {
+  label: string;
+  score: string;
+  icon: React.ReactElement;
+}
+
+const statData: StatData[] = [
+  {
+    label: 'Weekly Appointments',
+    score: '> 500',
+    icon: <BsPerson size={24} color="#FF5733" />,
+  },
+  {
+    label: 'Positive Reviews',
+    score: '4.9/5',
+    icon: <AiFillStar size={24} color="#f2de76" />,
+  },
+  {
+    label: 'Satisfied Patients',
+    score: ' > 11.2K',
+    icon: <FaUsers size={24} color="#68d391" />,
+  },
+  {
+    label: 'Twitter Followers',
+    score: '23K',
+    icon: <FaTwitter size={24} color="#3182ce" />,
+  },
+];
+
+const Stats = () => {
+  const textColor = useColorModeValue("gray.600", "gray.300");
+  const tealColor = useColorModeValue("teal.600", "teal.500");
+
+  return (
+    <Container maxW="5xl" p={{ base: 4, sm: 10 }}>
+      <Flex direction={{ base: 'column', md: 'row' }} justify="space-between">
+        <Stack spacing={4}>
+          <chakra.h1
+            fontSize="2xl"
+            lineHeight={1.2}
+            fontWeight="bold"
+            color={tealColor}
+          >
+            Our Achievements
+          </chakra.h1>
+          <Text fontSize="md" color={textColor} maxW="480px">
+            We take immense pride in our journey of providing top-notch
+            healthcare products and transforming lives. Our commitment to
+            excellence and patient satisfaction drives our success.
+          </Text>
+        </Stack>
+        <SimpleGrid columns={2} spacing={6} pt={8} pl={{ base: 0, md: 10 }}>
+          {statData.map((data, index) => (
+            <Stack
+              key={index}
+              p={4}
+              border="1px solid"
+              borderColor={textColor}
+              borderRadius="md"
+              align="center"
+              spacing={1}
+              bg={tealColor}
+            >
+              <Box fontSize="2xl" fontWeight="bold" color={'white'}>
+                {data.score}
+              </Box>
+              <Box fontSize="lg" color={'white'}>
+                {data.icon}
+              </Box>
+              <Text fontSize="md" color={'white'}>
+                {data.label}
+              </Text>
+            </Stack>
+          ))}
+        </SimpleGrid>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Stats;
+
---
+++ client/pages/index.tsx
@@ -0,0 +1,42 @@
+import { Box } from '@chakra-ui/react';
+import type { NextPage } from 'next';
+import ContactInfo from '../components/ContactInfo';
+import NavBar from '../components/NavBar';
+import Hero from '../components/Hero';
+import Stats from '../components/Stats';
+import AppointmentFeatures from '../components/AppointmentFeatures';
+import FAQ from '../components/Faq';
+import Testimonials from '../components/Testimonials';
+import MedicalCalculators from '../components/MedicalCalculators';
+import HealthTips from '../components/HealthTips';
+import Quotes from '../components/Quotes';
+import BookAppointment from '../components/BookAppointment';
+import Footer from '../components/Footer';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <main>
+        <Box as="main">
+        <ContactInfo />
+          <NavBar />
+          <Hero />
+          <Stats />
+          <AppointmentFeatures />
+          <FAQ />
+          <Testimonials />
+          <MedicalCalculators />
+          <HealthTips />
+          <Quotes />
+          <div id="BookAppointment">
+          <BookAppointment />
+          </div>
+          <Footer />
+        </Box>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
+
