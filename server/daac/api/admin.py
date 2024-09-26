ef1eea9164768a173be78a2f1d515e6af4fdf9d7
yusufshihata <yusufshihata2006@gmail.com>
2024-09-26 09:55:45 +0300

    ready

--- server/daac/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Product
-
-class ProductAdmin(admin.ModelAdmin):
-    fields = ['image', 'title', 'description', 'category', 'price']
-
-admin.site.register(Product, ProductAdmin)
--- server/daac/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views, test
-
-urlpatterns = [
-    path('products/', views.getData, name='products'),
-]
